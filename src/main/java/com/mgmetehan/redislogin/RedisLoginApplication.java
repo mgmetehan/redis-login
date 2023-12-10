7ba41a3594430b074f0f019f6c107dce458f7168
mgmetehan <mgmetehan@hotmail.com>
2023-12-10 19:23:47 +0300

    init project

---
+++ src/main/java/com/mgmetehan/redislogin/RedisLoginApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.redislogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLoginApplication.class, args);
+	}
+
+}
